e2232deb1b7961b11584284672cdb56f619ddc08
Mozillazg <opensource.mozillazg@gmail.com>
2013-03-11 23:24:24 +0800

    Added get_or_create_tag function.

--- flasksites/utils.py
+++ flasksites/utils.py
@@ -2,7 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from settings import db
+from models import Tag
 
 
 def init_db():
     db.create_all()
+
+
+def get_or_create_tag(tag_name):
+    tag = Tag.query.filter_by(name=tag_name).first()
+    if tag is None:
+        tag = Tag(name=tag_name)
+        db.session.add(tag)
+        db.session.commit()
+    return tag
