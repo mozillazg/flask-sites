a7f0a2d451c587f0838f13af99f873519d611e05
Mozillazg <opensource.mozillazg@gmail.com>
2013-03-26 14:18:23 +0800

    rewrite models.

--- flasksites/models.py
+++ flasksites/models.py
@@ -2,37 +2,82 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
-from random import choice
+import random
 import hashlib
 
 from settings import db
 
+__all__ = ['User', 'Site', 'Tag']
+
 
 class User(db.Model):
+    """base on june project(https://github.com/pythoncn/june)
+    """
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    nick_name = db.Column(db.String(80))
+
+    # for user: 1 - not verified, 2 - verified, > 20 staff > 40 admin
+    role = db.Column(db.Integer, default=1)
 
     is_admin = db.Column(db.Boolean, default=False)
-    token = db.Column(db.String(16))
+    token = db.Column(db.String(20))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     sites = db.relationship('Site', backref='submitted_by', lazy='dynamic')
 
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
+    def __init__(self, **kwargs):
+        self.token = self.create_token(16)
 
+        if 'password' in kwargs:
+            raw = kwargs.pop('password')
+            self.password = self.create_password(raw)
+
+        if 'username' in kwargs:
+            username = kwargs.pop('username')
+            self.username = username.lower()
+
+        if 'email' in kwargs:
+            email = kwargs.pop('email')
+            self.email = email.lower()
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __str__(self):
+        return self.nick_name or self.username
+
+    def __repr__(self):
+        return '<Account: %s>' % self.username
+
+    @staticmethod
+    def create_password(raw):
+        salt = User.create_token(8)
+        passwd = '%s%s%s' % (salt, raw,
+                             db.app.config['PASSWORD_SECRET'])
+        hsh = hashlib.sha1(passwd).hexdigest()
+        return "%s$%s" % (salt, hsh)
+
+    @staticmethod
     def create_token(length=16):
         chars = ('0123456789'
                  'abcdefghijklmnopqrstuvwxyz'
                  'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        salt = ''.join([choice(chars) for i in range(length)])
+        salt = ''.join([random.choice(chars) for i in range(length)])
+        return salt
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def check_password(self, raw):
+        if not self.password:
+            return False
+        if '$' not in self.password:
+            return False
+        salt, hsh = self.password.split('$')
+        passwd = '%s%s%s' % (salt, raw, db.app.config['PASSWORD_SECRET'])
+        verify = hashlib.sha1(passwd).hexdigest()
+        return verify == hsh
+
 
 tags = db.Table(
     'tags',
@@ -53,7 +98,7 @@ class Site(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     submitted_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow,
-                          onupdate=datetime.utcnow)
+                           onupdate=datetime.utcnow)
     # slug = db.Column(db.String(260), unique=True, nullable=False)
 
     def __repr__(self):
